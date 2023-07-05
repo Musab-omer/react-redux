6dd4bbb1adf081ea50ea878fbaac9eee0a046983
musab-omer <mosab.katry@gmail.com>
2023-07-05 11:00:27 +0300

    First Commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'mdb-ui-kit/css/mdb.min.css'
+
+import 'bootstrap/js/dist/collapse';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
---
+++ src/layout/MainNavbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+let MainNavbar = () => {
+    return (
+        <React.Fragment>
+            <div className="navbar navbar-expand-lg navbar-light bg-light shadow p-3">
+                <a href="/" className="navbar-brand p-2 text-danger fw-bolder">PizzaHut React and Redux</a>
+                <button className="navbar-toggler mx-3" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="mainNav">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a href="" className="nav-link">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="" className="nav-link">About</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+export default MainNavbar
