ad263b29e3051a9e828ab64721fdcbb6ca01285e
musab-omer <mosab.katry@gmail.com>
2023-07-06 12:49:54 +0300

    I connected root App component with redux
    using Prodvider of redux

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from './logo.svg';
 //import './App.css';
 import MainNavbar from './layout/MainNavbar';
 import PizzaHut from './component/PizzaHut';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <MainNavbar/>
-        <PizzaHut/>
+        <MainNavbar />
+        <Provider store={store}>
+          <PizzaHut />
+        </Provider>
+
       </header>
     </div>
   );
