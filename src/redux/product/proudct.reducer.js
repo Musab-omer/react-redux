6782eb501a8526fa5e1a9ce71b99369474c984e5
musab-omer <mosab.katry@gmail.com>
2023-07-07 23:24:04 +0300

    create and developed proudct reducer
    to modify and update proudct state throw proudct actions

---
+++ src/redux/product/proudct.reducer.js
@@ -0,0 +1,38 @@
+import * as proudctAction from "./proudct.actions"
+
+//feature key
+export const proudctFeatureKey = 'proudct'
+
+//initial stata
+let initialState = {
+    product: {
+        id: 'AA45BB#',
+        image: 'https://assets.swappie.com/cdn-cgi/image/width=600,height=600,dpr=2,fit=contain,format=auto/swappie-iphone-13-pro-max-gold.png?v=34',
+        name: 'Iphone 14 pro max',
+        price: 1039,
+        qty: 10
+    }
+}
+
+//reducer
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case proudctAction.DECRESE_QTY:
+            return {
+                product: {
+                    ...state.product,
+                    qty: (state.product.qty - 1 > 0) ? state.product.qty - 1 : 1
+                }
+            }
+            break;
+        case proudctAction.INCREASE_QTY:
+            return {
+                product: {
+                    ...state.product,
+                    qty: state.product.qty + 1
+                }
+            }
+            break;
+        default: return state
+    }
+}
