493295fbcd2282b4e682ff3b5a9aa7f784d0fcd1
musab-omer <mosab.katry@gmail.com>
2023-07-08 23:05:21 +0300

    car reducer

---
+++ src/redux/care/care.reducer.js
@@ -0,0 +1,21 @@
+import * as careAction from './care.actions'
+
+export const careFeatureKey='care';
+
+let initialState={
+    prand:'select car'
+}
+
+export const reducer=(state=initialState,action)=>{
+    let {type,payload}=action
+    switch (type) {
+        case careAction.CARE_PRAND:
+            return{
+                prand:payload
+            }
+            break;
+    
+        default: return state
+            break;
+    }
+}
