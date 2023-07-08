0eea0a15141c9342b888dde04354d912a084d920
musab-omer <mosab.katry@gmail.com>
2023-07-08 23:08:25 +0300

    Configure care reducer with root reducer

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,7 +1,6 @@
 import {combineReducers} from 'redux'
-
-
+import * as carReducer from './care/care.reducer'
 let rootReducer=combineReducers({
- 
+ [carReducer.carFeatureKey]:carReducer.reducer
 })
 export {rootReducer}
