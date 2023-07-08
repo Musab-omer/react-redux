b486e03ac4204e5b970fff3e1a88d499cd846f17
musab-omer <mosab.katry@gmail.com>
2023-07-08 22:15:45 +0300

    Configure CareSelector component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import MainNavbar from './layout/MainNavbar';
 import { Provider } from 'react-redux'
 import { store } from './redux/store';
+import CarSelector from './component/CarSelector'
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
       <header className="App-header">
         <MainNavbar />
         <Provider store={store}>
-         
+         <CarSelector/>
         </Provider>
       </header>
     </div>
