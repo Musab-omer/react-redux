646a08265cadce70bd40041e532d3a5b06434731
musab-omer <mosab.katry@gmail.com>
2023-07-08 23:42:17 +0300

    Configure message reducer with root reducer

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,6 +1,8 @@
-import {combineReducers} from 'redux'
-import * as carReducer from './care/care.reducer'
+import {combineReducers} from 'redux';
+import * as carReducer from './care/care.reducer';
+import * as messageReducer from './message/message.reducer';
 let rootReducer=combineReducers({
- [carReducer.carFeatureKey]:carReducer.reducer
+ [carReducer.carFeatureKey]:carReducer.reducer,
+ [messageReducer.messageFeatureKey]:messageReducer.reducer
 })
 export {rootReducer}
