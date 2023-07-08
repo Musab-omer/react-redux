f13668c1c6e8703b82e8a554b1015ca042916b86
musab-omer <mosab.katry@gmail.com>
2023-07-09 01:57:15 +0300

    root reducer

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import * as carReducer from './care/care.reducer';
 import * as messageReducer from './message/message.reducer';
+import * as registerReducer from './registration/register.reducer';
 let rootReducer=combineReducers({
  [carReducer.carFeatureKey]:carReducer.reducer,
- [messageReducer.messageFeatureKey]:messageReducer.reducer
+ [messageReducer.messageFeatureKey]:messageReducer.reducer,
+ [registerReducer.registerFeatureKey]:registerReducer.reducer
 })
 export {rootReducer}
