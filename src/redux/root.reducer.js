719c2cc87859139ce57b719482c9bd4e498ff4cc
musab-omer <mosab.katry@gmail.com>
2023-07-09 11:26:24 +0300

    configure hobbySelector reducer with root reducer

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,6 +1,7 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
+import * as hobbySelectorReducer from './hobies/hobbySelector.reducer';
 
-let rootReducer=combineReducers({
-
+let rootReducer = combineReducers({
+    [hobbySelectorReducer.hobiesFeatureKey]: hobbySelectorReducer.reducer
 })
-export {rootReducer}
+export { rootReducer }
