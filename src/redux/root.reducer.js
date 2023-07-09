2afd57da033418a8198d52639527fac2812e95df
musab-omer <mosab.katry@gmail.com>
2023-07-09 14:41:11 +0300

    configure hobbay selector reducer radio
     with root  reducer

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import * as hobbySelectorReducer from './hobies/hobbySelector.reducer';
+import * as hobbyRadioSelectorReducer from './hobby/hobbyRadioSelector.reducer'
 
 let rootReducer = combineReducers({
-    [hobbySelectorReducer.hobiesFeatureKey]: hobbySelectorReducer.reducer
+    [hobbySelectorReducer.hobiesFeatureKey]: hobbySelectorReducer.reducer,
+    [hobbyRadioSelectorReducer.hobiesFeatureKey]:hobbyRadioSelectorReducer.reducer
 })
 export { rootReducer }
