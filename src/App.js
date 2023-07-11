3373e04f23f7ade91c219bf0b06fac65caf00db7
musab-omer <mosab.katry@gmail.com>
2023-07-11 09:42:20 +0300

    Configure UserList component with app component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import MainNavbar from './layout/MainNavbar';
 import { Provider } from 'react-redux'
 import { store } from './redux/store';
+import UserList from './component/UserList';
 
 
 
@@ -13,7 +14,7 @@ function App() {
       <header className="App-header">
         <MainNavbar />
         <Provider store={store}>
-       
+          <UserList/>
         </Provider>
       </header>
     </div>
