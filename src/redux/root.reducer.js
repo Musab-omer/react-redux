94ff5f015d8a53a19864a7c4a20ec1f9b49049cc
musab-omer <mosab.katry@gmail.com>
2023-07-11 09:47:21 +0300

    Configure userList reducer with root reducer

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
-
+import * as usersListReducer from './users/userList.reducer';
 
 let rootReducer = combineReducers({
-    
+    [usersListReducer.userListFeatureKey]:usersListReducer.reducer
 })
 export { rootReducer }
