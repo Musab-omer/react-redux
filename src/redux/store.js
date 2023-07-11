dbf85c5bbf487cd5b6953dff18e24047a9b1c843
musab-omer <mosab.katry@gmail.com>
2023-07-11 09:45:21 +0300

    Configure redux thunk middlwar with redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,9 @@ import {createStore,applyMiddleware} from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import { rootReducer } from './root.reducer';
 import {logger} from 'redux-logger'
+import thunk from 'redux-thunk';
 
-const store=createStore(rootReducer,composeWithDevTools(applyMiddleware(logger)));
+let middleWars=[logger,thunk]
+
+const store=createStore(rootReducer,composeWithDevTools(applyMiddleware(...middleWars)));
 export {store}
