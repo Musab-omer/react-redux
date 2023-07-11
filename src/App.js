68bed211234cc378aeafe6bef247ccb73cfb85eb
musab-omer <mosab.katry@gmail.com>
2023-07-11 05:56:35 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import logo from './logo.svg';
 import MainNavbar from './layout/MainNavbar';
 import { Provider } from 'react-redux'
 import { store } from './redux/store';
-import Hobies from './component/Hobies';
-import HobiesRadio from './component/HobiesRadio';
-import EmployeeCard from './component/EmployeeCard';
-import AuthUser from './component/AuthUser';
-import Customers from './component/Customers';
+
 
 
 
@@ -17,11 +13,7 @@ function App() {
       <header className="App-header">
         <MainNavbar />
         <Provider store={store}>
-          {/* <Hobies/> */}
-          {/* <HobiesRadio/> */}
-          {/* <EmployeeCard/> */}
-          {/* <AuthUser/> */}
-          <Customers/>
+       
         </Provider>
       </header>
     </div>
--- src/redux/customers/customers.reducer.js
+++
@@ -1,9 +0,0 @@
-import {customerData} from '../../DataSource/customerStore'
-export const customersFeatureKey='customers'
-
-let initialStata={
-    customers:customerData
-}
-export const reducer =(state=initialStata)=>{
-    return state;
-}
