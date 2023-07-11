9269c009f4511e8d5832db4f49ccefb32e23b5ba
musab-omer <mosab.katry@gmail.com>
2023-07-11 18:12:41 +0300

    Configure contact app reducer with root reducer

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-
+import * as contactAppReducer from './contactApp/contactApp.reducer'
 let rootReducer = combineReducers({
-   
+   [contactAppReducer.contactAppFeatureKey]:contactAppReducer.reducer
 })
 export { rootReducer }
