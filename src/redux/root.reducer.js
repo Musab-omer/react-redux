f9d86f9effa4b02c2edebd859d7bb876909b1e7b
musab-omer <mosab.katry@gmail.com>
2023-07-11 13:46:41 +0300

    Configure CustomerCard Reducer with root Reducer

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import * as usersListReducer from './users/userList.reducer';
 import * as usersListSimpleReducer from './usersSimple/userListSimple.reducer'
+import * as customresCardReducer from './customersCard/customersCard.reducer'
 
 let rootReducer = combineReducers({
     [usersListReducer.userListFeatureKey]:usersListReducer.reducer,
-    [usersListSimpleReducer.userListSimpleFeatureKey]:usersListSimpleReducer.reducer
+    [usersListSimpleReducer.userListSimpleFeatureKey]:usersListSimpleReducer.reducer,
+    [customresCardReducer.customersCardFeaturKey]:customresCardReducer.reducer
 })
 export { rootReducer }
