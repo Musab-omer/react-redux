2c4d95625d7ac0957e5bae9ce1091b847b2a0115
musab-omer <mosab.katry@gmail.com>
2023-07-11 11:42:16 +0300

    Configured usersSimpleList reudcer with root reducer

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import * as usersListReducer from './users/userList.reducer';
+import * as usersListSimpleReducer from './usersSimple/userListSimple.reducer'
 
 let rootReducer = combineReducers({
-    [usersListReducer.userListFeatureKey]:usersListReducer.reducer
+    [usersListReducer.userListFeatureKey]:usersListReducer.reducer,
+    [usersListSimpleReducer.userListSimpleFeatureKey]:usersListSimpleReducer.reducer
 })
 export { rootReducer }
