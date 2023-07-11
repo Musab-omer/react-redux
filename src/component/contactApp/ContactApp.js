5c8ae3da23af6ff8b5fddd3b5909c6c691d3ec1e
musab-omer <mosab.katry@gmail.com>
2023-07-11 15:28:30 +0300

    ContactApp Main Component

--- src/component/contactApp/ContactApp.js
+++ src/component/contactApp/ContactApp.js
@@ -1,9 +1,32 @@
 import React from "react";
+import ContactsList from "./ContactsList";
+import ContactCard from "./ContactCard";
 
 let ContactApp=()=>{
     return(
         <React.Fragment>
-            <h2>Contact App</h2>
+           <section className="p-3">
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h2 className="text-primary">Contact App</h2>
+                        <p className="lead">Culpa veniam ea aliqua ipsum. Adipisicing ex culpa magna pariatur laboris labore adipisicing nostrud sit ut nulla minim proident. Nisi Lorem eiusmod quis pariatur consequat eu pariatur. Proident magna tempor id dolore elit proident. Exercitation officia amet enim enim cillum sunt ad aliqua ex proident magna quis ex dolor. Velit occaecat fugiat et aute nostrud labore nulla. Pariatur ullamco Lorem nostrud sit proident exercitation minim consequat anim eiusmod in ut.</p>
+                    </div>
+                </div>
+            </div>
+           </section>
+           <section className="p-3">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8">
+                        <ContactsList/>
+                    </div>
+                    <div className="col-md-4">
+                        <ContactCard/>
+                    </div>
+                </div>
+            </div>
+           </section>
         </React.Fragment>
     )
 }
