a1efe9e8265361f53e2ba862a150bad9612132e7
musab-omer <mosab.katry@gmail.com>
2023-07-12 16:00:51 +0300

    Configured githubSearch reducer with root reducer

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux'
-
+import * as githubSearchAppReducer from './gitHubApp/githubSearchApp.reducer'
 let rootReducer = combineReducers({
-
+    [githubSearchAppReducer.githubSearchAppFeatureKey]:githubSearchAppReducer.reducer
 })
 export { rootReducer }
