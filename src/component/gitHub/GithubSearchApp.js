a417f676812b0e16861f78fb513a5ecdee0da421
musab-omer <mosab.katry@gmail.com>
2023-07-12 14:38:53 +0300

    Configured GithubSearch component with GithubSearchApp

---
+++ src/component/gitHub/GithubSearchApp.js
@@ -0,0 +1,11 @@
+import React from "react";
+import GithubSearch from "./github-profile/GithubSearch";
+
+let GithubSearchApp=()=>{
+    return(
+        <React.Fragment>
+            <GithubSearch/>
+        </React.Fragment>
+    )
+}
+export default GithubSearchApp
