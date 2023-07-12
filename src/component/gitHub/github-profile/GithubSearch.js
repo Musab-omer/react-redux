85197ba65f6e4d7b49e5d0ee013f0c66e0e4ec98
musab-omer <mosab.katry@gmail.com>
2023-07-12 14:58:36 +0300

    Configure all gethub sub component and handel actions and state
    get state and configured prifile , card, repos components and disptach actions and get state

---
+++ src/component/gitHub/github-profile/GithubSearch.js
@@ -0,0 +1,106 @@
+import React, { useState } from "react"
+import GithubProfileCard from "./GithubProfileCard"
+import GithubProfileDetails from "./GithubProfileDetails"
+import GithubRepos from "../github-repo/GithubRespos"
+import { useDispatch, useSelector } from "react-redux"
+import * as githubSearchAppAction from '../../../redux/gitHubApp/githubSearchApp.actions'
+import { githubSearchAppFeatureKey } from '../../../redux/gitHubApp/githubSearchApp.reducer'
+import spinnerImage from '../../../assets/DzUd.gif'
+
+let GithubSearch = () => {
+
+    let [user, setUser] = useState("")
+    let updateUserInput = (event) => {
+        setUser(event.target.value)
+    }
+
+    let gitHubInof = useSelector((state) => {
+        return state[githubSearchAppFeatureKey]
+    })
+    let { loading, profile, repos, errorMessage } = gitHubInof
+    let dispatch = useDispatch();
+
+    let searchGithubUser = () => {
+        //alert(user)
+        dispatch(githubSearchAppAction.fetachGithubProfile(user));
+
+        dispatch(githubSearchAppAction.fetachGithubRepos(user))
+    }
+
+    return (
+        <React.Fragment>
+            <section className="p-3">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <h2 className="text-success">Github Profile Search</h2>
+                            <p className="lead">Pariatur labore exercitation magna officia ex qui velit. Incididunt sint incididunt magna minim consequat officia nulla et reprehenderit amet irure anim et eu. Aliquip consequat ea ad qui Lorem voluptate esse aliqua sint occaecat ipsum non proident. Laborum duis quis elit ea veniam. Dolor elit minim consequat voluptate nostrud.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="p-3">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <form className="row">
+                                <div className="col-auto">
+                                    <input type="text" className="form-control" value={user} onChange={updateUserInput} placeholder="user profile name" />
+                                </div>
+                                <div className="col-auto">
+                                    <button type="button" className="btn btn-success" onClick={searchGithubUser}>Search</button>
+                                </div>
+                            </form>
+                            {
+                                errorMessage != "" ?
+                                    <React.Fragment>
+                                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                            <strong>{errorMessage.message}</strong>  Github profile Not found please re enter correct name .
+                                            {/* <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
+                                        </div>
+                                    </React.Fragment> : null
+                            }
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div className="container">
+                    {
+                        loading &&
+                        <img src={spinnerImage} className="w-25 d-block m-auto" alt="" />
+                    }
+                    <div className="row">
+
+                        <div className="col-md-4">
+                            {
+                                Object.keys(profile).length > 0 ?
+                                    <GithubProfileCard /> : null
+                            }
+                        </div>
+                        <div className="col-md-8">
+                            {
+                                Object.keys(profile).length > 0 ?
+                                    <GithubProfileDetails /> : null
+                            }
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            {
+                                repos.length > 0 ?
+                                    <GithubRepos /> : null
+                            }
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <div style={{ marginBottom: "300px" }}></div>
+        </React.Fragment>
+    )
+}
+export default GithubSearch
