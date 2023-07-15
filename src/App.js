6b3120d724f6e106105527585aef9d4212d0b319
musab-omer <mosab.katry@gmail.com>
2023-07-16 01:15:11 +0300

    Configured all component with redux stor and route

--- src/App.js
+++ src/App.js
@@ -3,16 +3,34 @@ import logo from './logo.svg';
 import MainNavbar from './layout/MainNavbar';
 import { Provider } from 'react-redux'
 import { store } from './redux/store';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import Employees from './components/employee/Employees';
+import Employee from './components/employee/Employee';
+import Stacks from './components/stack/Stacks';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <MainNavbar />
+
+
         <Provider store={store}>
-        
+          <BrowserRouter>
+            <MainNavbar />
+            <Routes>
+              <Route exact path='/' Component={Home} />
+              <Route exact path='/employees/list' Component={Employees} />
+              <Route exact path='/employees/:id' Component={Employee} />
+              <Route exact path='/stacks/list' Component={Stacks} />
+              <Route exact path='/about' Component={About} />
+
+            </Routes>
+          </BrowserRouter>
         </Provider>
+
       </header>
     </div>
   );
