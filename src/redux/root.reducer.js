66d711918bfb3c417523f65b05f83035b8f6c49d
musab-omer <mosab.katry@gmail.com>
2023-07-16 01:17:37 +0300

    configured employee reducer with root reducer

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux'
-
+import * as employeeReducer from '../redux/employee/employee.reducer'
 let rootReducer = combineReducers({
-    
+    [employeeReducer.employeeFeatureKey]:employeeReducer.reducer
 })
 export { rootReducer }
