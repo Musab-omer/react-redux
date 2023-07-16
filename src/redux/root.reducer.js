2d6488d5337e177be4ded828001137866c669673
musab-omer <mosab.katry@gmail.com>
2023-07-16 16:23:17 +0300

    Refactored

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import * as employeeReducer from '../redux/employee/employee.reducer'
+import * as stacksReducer from './stock/stock.reducer'
 let rootReducer = combineReducers({
-    [employeeReducer.employeeFeatureKey]:employeeReducer.reducer
+    [employeeReducer.employeeFeatureKey]:employeeReducer.reducer,
+    [stacksReducer.stacksFeatureKey]:stacksReducer.reducer
 })
 export { rootReducer }
