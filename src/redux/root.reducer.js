3dfc55fdbade98c9f7d7dceca940d213fb52b620
musab-omer <mosab.katry@gmail.com>
2023-08-02 12:44:16 +0300

    Configured Stocks reducer with root reducer

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -3,6 +3,6 @@ import * as employeeReducer from '../redux/employee/employee.reducer'
 import * as stacksReducer from './stock/stock.reducer'
 let rootReducer = combineReducers({
     [employeeReducer.employeeFeatureKey]:employeeReducer.reducer,
-    [stacksReducer.stacksFeatureKey]:stacksReducer.reducer
+    [stacksReducer.stocksFeatureKey]:stacksReducer.reducer
 })
 export { rootReducer }
