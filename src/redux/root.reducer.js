4bb7b530d49f7a763efab7298817c7d37aa07c64
musab-omer <mosab.katry@gmail.com>
2023-08-02 18:21:01 +0300

    Configured product reducer with root reducer

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux'
-
+import * as productReducer from './product/product.reducer';
 let rootReducer = combineReducers({
-    
+    [productReducer.productFeatureKey]:productReducer.reducer
 })
 export { rootReducer }
