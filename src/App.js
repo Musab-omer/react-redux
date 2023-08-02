3a760a383756051fb7da0140d5445a743d4d0ac4
musab-omer <mosab.katry@gmail.com>
2023-08-02 13:49:53 +0300

    Configured component with app and router

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import MainNavbar from './layout/MainNavbar';
 import { Provider } from 'react-redux'
 import { store } from './redux/store';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/home/Home';
+import ProductsList from './components/product/ProductsList';
+import Admin from './components/admin/Admin';
+import UpdateProduct from './components/product/UpdateProduct';
+import CreateProduct from './components/product/CreateProduct';
 
 function App() {
   return (
@@ -15,7 +20,11 @@ function App() {
           <BrowserRouter>
             <MainNavbar />
             <Routes>
-              
+              <Route exact path='/' Component={Home}/>
+              <Route exact path='/products/list' Component={ProductsList}/>
+              <Route exact path='/products/admin' Component={Admin}/>
+              <Route exact path='/products/:id' Component={UpdateProduct}/>
+              <Route exact path='/products/create' Component={CreateProduct}/>
             </Routes>
           </BrowserRouter>
         </Provider>
